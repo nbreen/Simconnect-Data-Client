370d61275a4251138893c43e581ba0ecfc032d2c
BuildTools <unconfigured@null.spigotmc.org>
2020-12-11 00:38:26 -0600

    Small changes to server. Probably need to modify .h file when serializers are ready

---
+++ Server/server.h
@@ -0,0 +1,30 @@
+struct ObjectData
+{
+    char    szTitle[256];
+    double  dAbsoluteTime;
+    double  dTime;
+    int     uDayOfTheYear;
+    int     uYear;
+    int     uMonthOfTheYear;
+    int     uDayOfTheMonth;
+    int     uDayOfTheWeek;
+    int     uTimeZoneOffset;
+    double  dSimTime;
+    double  dLatitude;
+    double  dLongitude;
+    double  dAltitude;
+    double  dPitch;
+    double  dBank;
+    double  dHeading;
+    double  dVelocityX;
+    double  dVelocityY;
+    double  dVelocityZ;
+    double  dTemperature;
+    double  dAirPressure;
+    double  dAirDensity;
+    double  dWindVelocity;
+    double  dWindDirection;
+    double  dWindX;
+    double  dWindY;
+    double  dWindZ;
+};
